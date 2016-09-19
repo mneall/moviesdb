5295bd0286024b9e36438db6f760a4aed54eb50d
Matthew Neall <neallmr@gmail.com>
2016-09-19 10:06:17 -0700

    add form on index

--- apps/app1/models.py
+++ apps/app1/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Movies(models.Model):
-	title = models.CharField(max_length=100, )
+	title = models.CharField(max_length=100)
+	genre = models.CharField(max_length=25)
+	year = models.IntegerField(max_length=4)
+	created_at = models.DateTimeField()
